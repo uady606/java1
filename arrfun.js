94d5f8f802b88df5c9b97877a10a52c852034d09
uady606 <udaykiran21551@gmail.com>
2024-12-26 11:17:32 +0530

    new folder

--- arrfun.js
+++ arrfun.js
@@ -1,9 +1,33 @@
-const multiply= (a, b) => {
-    const res=a*b;
-    console.log(res);
+/*const multiply= (a, b) => {
 
+    console.log(a*b);
 }
+    
 multiply(4,8);
-multiply(4,0);
+multiply(4,0);*/
+
+
+/*const add= (a, b) => {
+
+   return a+b;
+}
+    console.log(add(1,2));*/
+
+    const multiparameters=(a,b)=>{
+        const add=a+b;
+        const sub=a-b;
+        const mul=a*b;
+
+        console.log(`add:${add}`);
+        console.log(`sub:${sub}`);
+        console.log(`mul:${mul}`);
+    }
+    multiparameters(3,10);
+    multiparameters(3,15);
+
+
+
+
+
 
 
